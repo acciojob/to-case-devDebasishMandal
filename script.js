6a2bab0c4bfaafbd3c3f828a50f7a31f6e0ac3b8
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2024-02-21 19:06:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function toCase(text) {
   // write your code here
+	let ans=`${text.toLowerCase()}-${text.toUpperCase()}`;
+	return ans;
 }
 
 // DO not change the code below
 
-const text = prompt("Enter text:");
-alert(toCase(text));
+// const text = prompt("Enter text:");
+// alert(toCase(text));
